bc12dfa6fd6b3342d49d919eb6827204022be0b1
Enzo Souza Duarte <enzosouzaduarte6@outlook.com>
2026-06-19 18:40:14 -0300

    alterado e concluído o código javascript, abrangendo uma manipulação maior do DOM

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-document.getElementById("contact-form").addEventListener("submit", (event) => {
+const form = document.getElementById("contact-form");
+const name = form.querySelector('input[type="text"]');
+const email = form.querySelector('input[type="email"]');
+const textarea = form.querySelector('textarea');
+const submitButton = form.querySelector("button");
+
+form.addEventListener("submit", (event) => {
 
         event.preventDefault();
-        alert("Mensagem enviada com sucesso!");
+        submitButton.textContent = "Mensagem enviada!";
+        submitButton.style.backgroundColor = "green";
+
+        form.reset();
+
 });
