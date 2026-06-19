55972b793dcc30d4e7192fa5fdd79466bf43e529
Enzo Souza Duarte <enzosouzaduarte6@outlook.com>
2026-06-19 08:07:06 -0300

    removido um button não funcional e corrigido o código javascript

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-const menuBtn = document.getElementById("menu-btn");
-const menu = document.getElementById("menu");
-
-menuBtn.addEventListener("click", () => {
-    menu.classList.toggle("active");
-});
-
 document.getElementById("contact-form").addEventListener("submit", (event) => {
 
         event.preventDefault();
